e1e855658372d4d7f0e62ec8a1f7384088296b9c
Rafe Hart <rafael.hart9@gmail.com>
2020-03-08 16:49:41 +1100

    Formatting

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # This file defines what the update script should do. 
 
 # Git Repositories
-# My preference is to install most external repositories under a single directory in my home folder, and to the same 
-# with other people's repositories in a separate directory. These will be prefixed with the user's home directory.
+# My preference is to install most external repositories under a single directory in my home folder,
+# and to the same with other people's repositories in a separate directory. These will be prefixed 
+# with the user's home directory.
 personal_repo_dir = '/x'
 external_repo_dir = '/z'
 
@@ -10,12 +11,18 @@ external_repo_dir = '/z'
 directories_to_remove = ['Documents', 'Music', 'Pictures', 'Public', 'Templates', 'Videos']
 
 # These packages will be installed from APT. 
-packages_to_install = ['most', 'ttf-mscorefonts-installer', 'pydf', 'htop',
+packages_to_install = ['most', 'ttf-mscorefonts-installer', 'pydf', 'htop', 'gobuster',
                        'exif', 'hexedit', 'python3-pip', 'python3-venv']
 
 # These python packages will be installed globally
 pip_packages = ['pipenv', 'pylint']
 
-# Last of all, take a look in the /scripts directory. Every script ending in .sh or .py will be run, provided it's
-# executable. For example, the current scripts install VS Code and Typora. Any script that goes in this directory 
-# should be written so it can run multiple times without causing problems.
+# These git  repositories will be synced to the 'external repo' directory
+ext_repositories_to_sync = ['https://github.com/danielmiessler/SecLists',
+                            'https://github.com/SecureAuthCorp/impacket',
+                            'https://github.com/swisskyrepo/PayloadsAllTheThings']
+
+# Last of all, take a look in the /scripts directory. Every script ending in .sh or .py will be run,
+# provided it's # executable. For example, the current scripts install VS Code and Typora. Any 
+# script that goes in this directory should be written so it can run multiple times without causing 
+# problems.
