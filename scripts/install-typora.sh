0522b1f064ab42cbe76ee3407594a6c77d5f5307
Rafe Hart <rafael.hart9@gmail.com>
2020-03-08 16:03:16 +1100

    Adding some install scripts for external programs

---
+++ scripts/install-typora.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+GREEN="\033[1;32m"
+GREY="\033[0;37m"
+ENDCOLOR="\e[0m"
+
+# Check if typora is installed
+if [ $(sudo dpkg-query -W -f='${Status}' typora 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+    echo -ne $GREEN">>> "$ENDCOLOR; echo "Installing Typora"
+
+    # Add Typora's key and repository
+    wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+    echo -e "\ndeb https://typora.io/linux ./" | sudo tee -a /etc/apt/sources.list
+    sudo apt update
+
+    # Install typora
+    sudo apt install typora
+else 
+    echo -ne $GREY">>> "$ENDCOLOR; echo "Package 'typora' already installed"
+fi
---
+++ scripts/install-vscode.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+GREEN="\033[1;32m"
+GREY="\033[0;37m"
+ENDCOLOR="\e[0m"
+
+# Check if typora is installed
+if [ $(sudo dpkg-query -W -f='${Status}' code 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+    echo -ne $GREEN">>> "$ENDCOLOR; echo "Installing VS Code"
+
+    cd ~
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+    rm ~/packages.microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt install -y apt-transport-https
+    sudo apt update -y
+    sudo apt install -y code
+else 
+    echo -ne $GREY">>> "$ENDCOLOR; echo "Package 'code' already installed"
+fi
+
