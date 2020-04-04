1bea14962fd5d13a5c392541e50176e513d22417
Rafe Hart <rafael.hart9@gmail.com>
2020-04-04 21:46:09 +1100

    Install Chrome

--- scripts/install-massdns.sh
+++ scripts/install-massdns.sh
@@ -3,7 +3,7 @@ GREEN="\033[1;32m"
 GREY="\033[0;37m"
 ENDCOLOR="\e[0m"
 
-# Check if MassDNSChrome is installed
+# Check if MassDNS is installed
 if [ ! -d "/opt/massdns" ]
 then
     echo -ne $GREEN">>> "$ENDCOLOR; echo "Installing massdns"
