435b5def469a2d8e97b03382ae775e683bd02362
Rafe Hart <rafael.hart9@gmail.com>
2020-08-11 19:48:35 +1000

    Extract rockyou.txt.gz if installed

---
+++ scripts/extract-rockyou.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+GREEN="\033[1;32m"
+ENDCOLOR="\e[0m"
+
+# Check rockyou.txt.gz exists
+if [ -f "/usr/share/wordlists/rockyou.txt.gz" ]
+then
+    # Check that it hasn't already been unzipped
+    if [ ! -f "/usr/share/wordlists/rockyou.txt" ]
+    then
+        echo -ne $GREEN">>> "$ENDCOLOR; echo "Extracting rockyou.txt"
+        sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
+fi
