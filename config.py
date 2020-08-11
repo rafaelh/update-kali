d7f7dc708b1b84e44c96e854982606a83beba5ab
Rafe Hart <rafael.hart9@gmail.com>
2020-08-11 20:34:02 +1000

    Adding additional packages

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ directories_to_remove = ['Documents', 'Music', 'Pictures', 'Public', 'Templates'
 # These kali packages will be installed
 packages_to_install = ['most', 'ttf-mscorefonts-installer', 'pydf', 'htop', 'gobuster', 'amass',
                        'golang', 'exif', 'hexedit', 'jq', 'python3-pip', 'python3-venv',
-                       'apt-transport-https', 'curl', 'filezilla', 'meld']
+                       'apt-transport-https', 'curl', 'filezilla', 'meld',
+                       'net-tools', 'tmux']
 
 # These kali packages will be removed
 packages_to_remove = []
