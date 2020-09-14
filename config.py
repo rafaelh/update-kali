362a9881decc2a36f99b346fe8aaa1f36d8c5b79
Rafe Hart <rafael.hart9@gmail.com>
2020-09-14 14:52:52 +1000

    Adding DNSrecon dependencies

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ directories_to_remove = ['Documents', 'Music', 'Pictures', 'Public', 'Templates'
 # These kali packages will be installed
 packages_to_install = ['most', 'ttf-mscorefonts-installer', 'pydf', 'htop', 'gobuster', 'amass',
                        'golang', 'exif', 'hexedit', 'jq', 'python3-pip', 'python3-venv',
-                       'apt-transport-https', 'curl', 'filezilla', 'meld',
-                       'ncat', 'net-tools', 'tmux', 'steghide', 'bash-completion']
+                       'apt-transport-https', 'curl', 'filezilla', 'meld', 'ncat', 'net-tools',
+                       'tmux', 'steghide', 'bash-completion', 'ieee-data', 'python3-netaddr']
 
 # These kali packages will be removed
 packages_to_remove = ['zsh', 'zsh-syntax-highlighting', 'zsh-autosuggestions']
