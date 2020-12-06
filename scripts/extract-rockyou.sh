9a691d572f327a59c32baef363c8418ede0617b5
Rafe Hart <rafael.hart9@gmail.com>
2020-12-06 12:03:21 +1100

    Create UTF-8 version for Kerberoast

--- scripts/extract-rockyou.sh
+++ scripts/extract-rockyou.sh
@@ -14,5 +14,11 @@ then
         echo -ne $GREEN"[+] "$ENDCOLOR; echo "Extracting rockyou.txt"
         sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
     fi
+    # Check that we have a UTF-8 version for kerberoast, etc
+    if [ ! -f "/usr/share/wordlists/rockyou_utf8.txt" ]
+    then
+        echo -ne $GREEN"[+] "$ENDCOLOR; echo "Creating UTF-8 version of rockyou.txt"
+        sudo iconv -f ISO-8859-1 -t UTF-8 /usr/share/wordlists/rockyou.txt > /usr/share/wordlists/rockyou_utf8.txt
+    fi
 fi
 
