98494138daf213a48416582e9bbc1eb000b69f24
Rafe Hart <rafael.hart9@gmail.com>
2021-01-23 14:48:09 +1100

    Update cloud_enum if installed

--- scripts/install-cloud_enum.sh
+++ scripts/install-cloud_enum.sh
@@ -8,8 +8,17 @@ ENDCOLOR="\e[0m"
 # Check if cloud_enum is installed
 if [ ! -d "/opt/cloud_enum" ]
 then
-    echo -ne $GREEN"[+] "$ENDCOLOR; echo "Installing cloud_enum.py"
+    echo -ne $GREEN"[+] "$ENDCOLOR; echo "Installing cloud_enum"
     mkdir /opt/cloud_enum
     git clone https://github.com/initstring/cloud_enum /opt/cloud_enum
     sudo ln -s /opt/cloud_enum/cloud_enum.py /usr/local/bin/cloudenum
+fi
+
+# If it's already installed, check for updates
+if [ -d "/opt/cloud_enum" ]
+then
+    echo -ne $GREEN"[+] "$ENDCOLOR; echo "Checking for cloud_enum updates"
+    cd /opt/cloud_enum
+    git pull
+    cd - 1>/dev/null
 fi
