3001c5583accf0f3c897e8792f2204246499ad30
Rafe Hart <rafael.hart9@gmail.com>
2021-01-24 21:29:55 +1100

    Update massdns

--- scripts/install-massdns.sh
+++ scripts/install-massdns.sh
@@ -11,7 +11,24 @@ then
     echo -ne $GREEN"[+] "$ENDCOLOR; echo "Installing massdns"
     git clone https://github.com/blechschmidt/massdns /opt/massdns
     cd /opt/massdns
-    sudo make
+    make
     sudo ln -s /opt/massdns/bin/massdns /usr/local/bin/massdns
     cd - 1>/dev/null
 fi
+
+# If it's already installed, check for updates
+if [ -d "/opt/massdns" ]
+then
+    cd /opt/massdns
+    git remote update 1>/dev/null
+    HEADHASH=$(git rev-parse HEAD)
+    UPSTREAMHASH=$(git rev-parse master@{upstream})
+    if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
+        # Actions to perform if there are remote updates
+        echo -ne $GREEN"[+] "$ENDCOLOR; echo "Updating massdns"
+        git pull
+        make
+    fi
+    cd - 1>/dev/null
+fi
+
