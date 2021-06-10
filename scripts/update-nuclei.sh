6d139d59dc88716ba7d49a7a4081a728d2fc09a9
Rafe Hart <rafael.hart9@gmail.com>
2021-06-10 22:06:44 +1000

    Fix: Added colourization

--- scripts/update-nuclei.sh
+++ scripts/update-nuclei.sh
@@ -2,8 +2,12 @@
 set -Eeuo pipefail
 trap "echo -e \"\033[1;31m[!] \e[0m Script error occured.\"" ERR
 
+YELLOW="\033[1;33m"
+ENDCOLOR="\e[0m"
+
 # Update nuclei templates if installed
 if command -v nuclei &> /dev/null
 then
+    echo -ne $YELLOW"[>] "$ENDCOLOR; echo "Updating Nuclei templates"
     nuclei -update-templates
 fi
